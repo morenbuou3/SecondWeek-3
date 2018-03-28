367d00aee210313226c58e97ae5aff15bc7249fc
king <369558172@qq.com>
2018-03-28 15:56:17 +0800

    [ADD-METHOD] SECTION1 PracticeA

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        if (collection1 != null && collection2 != null) {
+            List<String> list = new ArrayList<>(collection1);
+            List<String> list2 = new ArrayList<>(collection2);
+            list.retainAll(list2);
+            return list;
+        }
+        return Collections.emptyList();
     }
 }
