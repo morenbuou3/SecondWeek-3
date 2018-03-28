63b8eceec5a11298d429e729171e5ef2acca235c
king <369558172@qq.com>
2018-03-28 15:58:44 +0800

    [ADD-METHOD] SECTION1 PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,30 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.List;
+import java.util.*;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        /*去除重复元素*/
+        Set<String> set = new LinkedHashSet<>();
+        if (collection1 != null && collection2 != null) {
+            for (String aCollection1 : collection1) {
+                boolean flag = false;
+                for (List<String> aCollection2 : collection2) {
+                    for (String anACollection2 : aCollection2) {
+                        if (aCollection1.equals(anACollection2)) {
+                            set.add(anACollection2);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        break;
+                    }
+                }
+            }
+            return new ArrayList<>(set);
+        }
+        return Collections.emptyList();
     }
 }
