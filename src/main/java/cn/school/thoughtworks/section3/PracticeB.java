672a67b0024d3c75e645413b8e7066456bde42ce
king <369558172@qq.com>
2018-03-28 16:01:03 +0800

    [ADD-METHOD] SECTION3 PracticeB

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        if (collectionA != null && object != null) {
+            for (String key : collectionA.keySet()) {
+                if (object.get("value").contains(key)) {
+                    Integer number = collectionA.get(key);
+                    collectionA.put(key, number - number / 3);
+                }
+            }
+        }
+        return collectionA;
     }
 }
