edf92dbef423012392ba6c793a76f9e4b3cd3f97
king <369558172@qq.com>
2018-03-28 16:00:48 +0800

    [ADD-METHOD] SECTION3 PracticeA

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,19 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        if (collectionA != null && object != null) {
+            for (String key : collectionA.keySet()) {
+                if (object.get("value").contains(key)) {
+                    collectionA.put(key, collectionA.get(key) - 1);
+                }
+            }
+        }
+        return collectionA;
     }
 }
