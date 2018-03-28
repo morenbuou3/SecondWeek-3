9530b1877930a1f188f9a56de17d23d6de10044c
king <369558172@qq.com>
2018-03-28 16:01:19 +0800

    [ADD-METHOD] SECTION3 PracticeC

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,17 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        PracticeA section2PracticeA = new PracticeA();
+        PracticeB section3PracticeB = new PracticeB();
+        Map<String, Integer> collection3 = section2PracticeA.countSameElements(collectionA);
+        collection3 = section3PracticeB.createUpdatedCollection(collection3, object);
+        return collection3;
     }
 }
